2292db8444731153931c20bc25388767365b49c7
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-02-28 16:45:47 -0700

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :departments do
     resources :items
   end
+
+  scope 'items/:item_id', as: 'item' do
+    resources :comments, only: [:new, :create]
+  end
 end
